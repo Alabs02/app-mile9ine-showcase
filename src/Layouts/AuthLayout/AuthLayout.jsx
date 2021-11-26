cf6713d0e23a122121764bb68c3655b786e57101
Alabs02 <alabson.inc@gmail.com>
2021-11-26 13:18:44 +0100

    new changes

--- src/Layouts/AuthLayout/AuthLayout.jsx
+++ src/Layouts/AuthLayout/AuthLayout.jsx
@@ -10,13 +10,13 @@ const AuthLayout = forwardRef(({ children }, ref) => (
           <div className="container h-100">
             <div className="row justify-content-center h-100 align-items-center">
               <div className="col-md-6 col-sm-12">
-                <div className="authincation-content">
+                <div className="authincation-content bg-dark">
                   <div className="row no-gutters">
                     <div className="col-xl-12">
                       <div className="auth-form">
                         <div className="mb-3 text-center">
                           <Link to="/">
-                            <img src="/images/logo-full.png" alt="logo" />
+                            <img src="/images/logo-full1.png" alt="logo" />
                           </Link>
                         </div>
                         {children && children}
